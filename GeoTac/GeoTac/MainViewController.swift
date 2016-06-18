fd426c869fdb5d3a32aec50d7dc304836aae394d
Kevia <cloud@kevia.me>
2016-06-18 02:57:13 -0700

    Adding in all the dependacies I can't spell

---
+++ GeoTac/GeoTac/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  GeoTac
+//
+//  Created by Ace_Rimmer on 6/18/16.
+//  Copyright © 2016 HackTacoma. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
---
+++ GeoTac/Pods/Target Support Files/Pods-GeoTac/Pods-GeoTac-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GeoTac : NSObject
+@end
+@implementation PodsDummy_Pods_GeoTac
+@end
